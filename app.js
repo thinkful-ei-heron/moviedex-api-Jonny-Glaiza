88c506cca9557159e7521e213e3dce162949b02f
Jonny Deates <48420377+JonnyDeates@users.noreply.github.com>
2019-10-29 12:31:15 -0600

    Update! Finished
    Co-Authored-By: Glaiza W <glaizawagner@users.noreply.github.com>

--- app.js
+++ app.js
@@ -1,16 +1,36 @@
 /* eslint-disable no-console */
+require('dotenv').config();
+const API_TOKEN = process.env.API_SECRET;
 const express = require('express');
+const morgan = require('morgan');
 const cors = require('cors');
+const helmet = require('helmet');
 
 const playstore = require('./movies-data-small');
 
 const app = express();
 
+app.use(morgan('common'));
+app.use(cors());
+app.use(helmet());
+
+function validateBearer(req, res, next) {
+    const authVal = req.get('Authorization') || '';
+    if (!authVal.startsWith('Bearer ')) {
+        return res.status(400).json({error: 'Authorization token not found'})
+    }
+    const [bearer ,token] = authVal.split(' ');
+    if(token !== API_TOKEN) {
+        return res.status(401).json({error: 'Token is invalid'})
+    }
+    next();
+}
+
 app.get('/', (req, res) => {
     res.send('Hello Express!');
 });
 
-app.get('/movie', (req, res) => {
+app.get('/movie', validateBearer, (req, res) => {
     let apps = playstore;
     const {genre, country, avg_vote} = req.query;
 
@@ -31,6 +51,5 @@ app.get('/movie', (req, res) => {
 
 
 app.listen(8000, () => {
-    app.use(cors());
     console.log('Express server is listening on port 8000!');
 });
