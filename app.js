e60c00e2a8b54eb1944941f77e3b670e911572f3
Glaiza Wagner <glaizawagner@gmail.com>
2019-10-29 09:28:37 -0700

    #1 Users can search for movies

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 const express = require('express');
 const cors = require('cors');
 
@@ -9,34 +10,27 @@ app.get('/', (req, res) => {
     res.send('Hello Express!');
 });
 
-app.get('/apps', (req, res) => {
+app.get('/movie', (req, res) => {
     let apps = playstore;
-    const {sort, genres} = req.query;
+    const {genre, country, avg_vote} = req.query;
 
-    if (sort) {
-        if ((!['app', 'rating'].includes(sort.toLowerCase()))) {
-            return res.status(400).send('Please provide a valid Sort');
-        }
-        apps.sort((a, b) => {
-            let word = sort.toLowerCase().substring(0,1).toUpperCase()+sort.toLowerCase().substring(1);
-            return a[word] > b[word] ? 1 : a[word] < b[word] ? -1 : 0;
-        });
-        if(sort.toLowerCase().includes('rating')){
-            apps.reverse();
-        }
+    if (genre) {
+        apps = apps.filter(app => app.genre.toLowerCase().includes(genre.toLowerCase()));
+    } 
+
+    if (country) {
+        apps = apps.filter(app => app.country.toLowerCase().includes(country.toLowerCase()));
+    } 
+
+    if (avg_vote) {
+        apps = apps.filter(app => app.avg_vote >= avg_vote);
     }
-    if (genres) {
-        if (['action', 'puzzle', 'strategy', 'casual', 'arcade', 'card'].includes(genres.toLowerCase())) {
-            apps = apps.filter(app => app.Genres.toLowerCase().includes(genres.toLowerCase()));
-        } else {
-            return res.status(400).send('Please provide a valid Genre');
-        }
-    }
+    
     res.json(apps);
 });
 
 
-app.listen(8000, (req, res) => {
+app.listen(8000, () => {
     app.use(cors());
     console.log('Express server is listening on port 8000!');
 });
